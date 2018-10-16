655d05e86902807a493cc04178bee7c75de55d7f
ibigfoot <tsellers@salesforce.com>
2018-10-16 15:28:08 +1100

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource\nhere in singapore\n');
+  res.send('respond with a resource\nhere is my change\n');
 });
 
 module.exports = router;
