880bb78031ec7c9b4467020499ab09d5fb5fa85d
samcfman <36816578+samcfman@users.noreply.github.com>
2018-11-13 10:40:33 +0800

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource\nhere is another changed\n');
+  res.send('respond with a resource\nwelcome to the fintech festivaln');
 });
 
 module.exports = router;
